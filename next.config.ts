be821269cdfa02e547b0b3e84c671ae19d7b0122
Nafis <nafisislam161@gmail.com>
2026-05-15 23:50:10 +0600

    image soucce

--- next.config.ts
+++ next.config.ts
@@ -3,28 +3,34 @@
 module.exports = {
   experimental: {
     serverActions: {
-      bodySizeLimit: '200mb',
+      bodySizeLimit: "200mb",
     },
   },
   images: {
     unoptimized: true,
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'picsum.photos',
+        protocol: "https",
+        hostname: "picsum.photos",
       },
       {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '8000',
-        pathname: '/storage/**',
+        protocol: "http",
+        hostname: "localhost",
+        port: "8000",
+        pathname: "/storage/**",
       },
       {
-        protocol: 'http',
-        hostname: '127.0.0.1',
-        port: '8000',
-        pathname: '/storage/**',
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "8000",
+        pathname: "/storage/**",
+      },
+      {
+        protocol: "http",
+        hostname: "148.135.136.43",
+        port: "8080",
+        pathname: "/storage/**",
       },
     ],
   },
-}
+};
